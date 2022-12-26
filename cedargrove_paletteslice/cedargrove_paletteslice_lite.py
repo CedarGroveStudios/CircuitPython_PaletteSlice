0bbc46106821d3607f0e58481ef4c443e52faeca
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-12-26 10:15:16 -0800

    create lite version

---
+++ cedargrove_paletteslice/cedargrove_paletteslice_lite.py
@@ -0,0 +1,141 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 JG for Cedar Grove Maker Studios
+# SPDX-License-Identifier: MIT
+"""
+`cedargrove_paletteslice_lite`
+================================================================================
+PaletteSlice (lite) is a CircuitPython wrapper class to add list slice capability to a
+displayio.Palette object while preserving transparency values. Creates a sliced
+displayio.Palette object.
+
+* Author(s): JG
+
+Implementation Notes
+--------------------
+**Hardware:**
+
+**Software and Dependencies:**
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+"""
+
+import displayio
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/CedarGroveStudios/CircuitPython_PaletteSlice.git"
+
+
+class PaletteSlice:
+    """A CircuitPython wrapper class to add list slice capability to a displayio.Palette
+    object while preserving transparency values."""
+
+    def __init__(self, source_palette):
+        """Instantiate the palette slice class. Creates a reference list and a
+        displayio.Palette object with source palette color values. Transparency is preserved.
+
+        param displayio.Palette source_palette: The source displayio palette object."""
+        self._source_palette = source_palette
+
+        # Create a clean new_palette
+        self._new_palette = displayio.Palette(len(self._source_palette))
+
+        # Create reference_list and new_palette copy using the source color and transparency
+        self._reference_list = []
+
+        for idx, color in enumerate(self._source_palette):
+            # Create reference_list with color and transparency
+            self._reference_list.append(
+                (color, self._source_palette.is_transparent(idx))
+            )
+            # Add color to new_palette
+            self._new_palette[idx] = color
+            # Set new_palette color index transparency
+            if self._source_palette.is_transparent(idx):
+                self._new_palette.make_transparent(idx)
+
+    def __getitem__(self, key):
+        """Returns a new_palette slice from reference_list.
+
+        param slice key: The slice object specifying the new palette."""
+
+        # Move reference_list slice into working_list
+        working_list = self._reference_list[key]
+        self._create_new_palette(working_list)
+        return self._new_palette
+
+    def __setitem__(self, key, value):
+        """Replace or add new color palette to a sliced new_palette.
+
+        param slice key: The target slice object for creating new_palette.
+        param displayio.Palette value: The palette of new colors."""
+
+        # Extract color and transparency from new_color palette and create working_list
+        working_list = []
+        for idx, color in enumerate(value):
+            working_list.append((color, value.is_transparent(idx)))
+
+        # Move working_list into the specified reference_list slice
+        self._reference_list[key] = working_list
+        self._create_new_palette(self._reference_list)
+
+    def __delitem__(self, key):
+        """UNTESTED:
+        Delete a slice from the primary class displayio.Palette. Permanently removes
+        the slice from reference_list and creates a new_palette.
+        Usage is del PaletteSlice[key]. ONLY WORKS IN REPL
+
+        param slice key: The target slice object to delete from the new color palette."""
+        """self._reference_list = del self._reference_list[key]
+        self._create_new_palette(self._reference_list)"""
+        # pylint: disable = (unnecessary-pass)
+        pass
+
+    def __len__(self):
+        return len(self._new_palette)
+
+    @property
+    def palette(self):
+        """The primary class palette (adjusted displayio.Palette object)."""
+        return self._new_palette
+
+    @property
+    def reference_list(self):
+        """A list of color, transparency tuples from the primary class palette."""
+        return self._reference_list
+
+    def is_transparent(self, index):
+        """Returns True if the palette index is transparent. Returns False if opaque.
+        Usage: PaletteSlice.is_transparend(index)
+
+        param int index: The palette color index to test."""
+        return self._reference_list[index][1]
+
+    def make_transparent(self, index):
+        """Set a palette index to transparency. Permanently modifies reference_list
+        and new_palette.
+        Usage: PaletteSlice.make_opaque(index)
+
+        param int index: The palette color index to be made transparent."""
+        self._reference_list[index] = (self._reference_list[index][0], True)
+        self._create_new_palette(self._reference_list)
+
+    def make_opaque(self, index):
+        """Set a palette index to opaque. Permanently modifies reference_list
+        and new_palette.
+        Usage: PaletteSlice.make_opaque(index)
+
+        param int index: The palette color index to be made opaque."""
+        self._reference_list[index] = (self._reference_list[index][0], False)
+        self._create_new_palette(self._reference_list)
+
+    def _create_new_palette(self, source_list):
+        """Create new_palette from a source list composed of color, transparency tuples."""
+        # Create a clean new_palette
+        self._new_palette = displayio.Palette(len(source_list))
+        # Add contents to new_palette using sliced source_list color and transparency
+        for idx, (color, transparency) in enumerate(source_list):
+            # Add color to new_palette
+            self._new_palette[idx] = color
+            if transparency:
+                # Set new_palette color index transparency
+                self._new_palette.make_transparent(idx)
---
+++ examples/paletteslice_simpletest_lite.py
@@ -0,0 +1,80 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 JG for Cedar Grove Maker Studios
+# SPDX-License-Identifier: MIT
+"""
+`paletteslice_simpletest_lite`
+================================================================================
+A test of the PaletteSlice (lite) wrapper class.
+
+* Author(s): JG
+"""
+
+import time
+import random
+import board
+import terminalio
+import displayio
+import adafruit_imageload
+from adafruit_display_text.label import Label
+from cedargrove_paletteslice import PaletteSlice
+
+BKG_IMAGE_FILE = "jp_hohimer.bmp"
+
+
+def print_list(new_list):
+    print("print_list object:", new_list)
+    print("print_list object length:", len(new_list))
+    for i, (color, transparency) in enumerate(new_list):
+        print(f"index: {i:03.0f} color: {color:#08x} transparency: {transparency}")
+
+
+# Define the display and primary display group
+display = board.DISPLAY
+display.brightness = 0.05
+primary_group = displayio.Group()
+
+# Display the test image and source color palette
+test_bitmap, test_palette_source = adafruit_imageload.load(
+    BKG_IMAGE_FILE, bitmap=displayio.Bitmap, palette=displayio.Palette
+)
+# Instantiate a sliceable copy of the reference palette
+pal_sliceable = PaletteSlice(test_palette_source)
+
+# Test of __get__
+
+# Test of __set__
+
+# Place the test image into a tile and append to the primary display group
+test_tile = displayio.TileGrid(test_bitmap, pixel_shader=test_palette_source)
+primary_group.append(test_tile)
+
+# Define on-screen label for slice object and palette length
+slice_label = Label(
+    terminalio.FONT, text="PALETTE SLICE TESTER: __getitem__", color=0xFFFFFF
+)
+slice_label.anchor_point = (0, 0.5)
+slice_label.anchored_position = (10, 225)
+primary_group.append(slice_label)
+
+# Show the test image and label
+display.show(primary_group)
+time.sleep(2)
+
+while True:
+    # Create a random slice object; prohibit step == 0
+    start = random.randrange(0, 255)
+    stop = random.randrange(0, 255)
+    step = random.randrange(-5, 6)
+    if step == 0:
+        step = 1
+
+    # Slice the palette and use for bkg_tile
+    test_tile.pixel_shader = pal_sliceable[start:stop:step]
+
+    if len(pal_sliceable.palette) != 0:
+        # Display slice object and pause to view
+        slice_label.text = f"[{start}:{stop}:{step}]  \nLENGTH={len(pal_sliceable)}"
+        time.sleep(0.75)
+
+print("\nfin\n")
+while True:
+    time.sleep(0.1)
