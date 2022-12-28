7315e3ae1037935437292f90ee2d620f72583ec7
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-12-28 14:55:19 -0800

    update package and examples nomenclature

--- examples/paletteslice_simpletest_acme.py
+++ examples/paletteslice_simpletest_acme.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 JG for Cedar Grove Maker Studios
 # SPDX-License-Identifier: MIT
 """
-`paletteslice_simpletest_lite`
+`paletteslice_simpletest_acme`
 ================================================================================
-A test of the PaletteSlice (lite) wrapper class.
+A test of the PaletteSlice wrapper class.
 
 * Author(s): JG
 """
@@ -16,7 +16,7 @@ import displayio
 from ulab import numpy as np
 import adafruit_imageload
 from adafruit_display_text.label import Label
-from cedargrove_paletteslice.cedargrove_paletteslice_lite import PaletteSlice
+from cedargrove_paletteslice.cedargrove_paletteslice import PaletteSlice
 
 BKG_IMAGE_FILE = "orchid.bmp"
 
@@ -46,7 +46,6 @@ primary_group = displayio.Group()
 test_bitmap, test_palette_source = adafruit_imageload.load(
     BKG_IMAGE_FILE, bitmap=displayio.Bitmap, palette=displayio.Palette
 )
-
 # Instantiate a sliceable copy of the reference palette
 pal_sliceable = PaletteSlice(test_palette_source)
 
@@ -69,6 +68,74 @@ step = 1  # Slice step
 print(f"  slice object: [{start}:{stop}:{step}]")
 pal_sliceable[start:stop:step] = get_palette
 print_palette(pal_sliceable[0:8])
+
+# Test of append()
+print("\n" + ("=" * 15))
+print("TEST append()")
+test = 0xF0F0F0
+print(f"  value to insert: {test:#08x}")
+
+(last_color, last_transparency) = pal_sliceable.reference_list[-1]
+length = len(pal_sliceable)
+print(
+    f"length BEFORE append: {length} last item: {last_color:#08x} {last_transparency}"
+)
+
+pal_sliceable.append(test)
+
+(last_color, last_transparency) = pal_sliceable.reference_list[-1]
+length = len(pal_sliceable)
+print(
+    f"length  AFTER append: {length} last item: {last_color:#08x} {last_transparency}"
+)
+
+# Test of is_transparent(), make_transparent(), make_opaque()
+print("\n" + ("=" * 15))
+test = -1  # Index to test
+print("TEST is_transparent(), make_transparent(), make_opaque()")
+print(f"  index value for test: {test}")
+print(f"  is_transparent: {pal_sliceable.is_transparent(test)}")
+pal_sliceable.make_transparent(test)
+print(f"AFTER make_transparent -> is_transparent: {pal_sliceable.is_transparent(test)}")
+pal_sliceable.make_opaque(test)
+print(f"AFTER make_opaque      -> is_transparent: {pal_sliceable.is_transparent(test)}")
+
+# Test of count()
+print("\n" + ("=" * 15))
+print("TEST count()")
+test = 0xFFFFFF  # Color value for search
+print(f"  color value for search: {test:#08x}")
+
+print(f"number of occurrences: {pal_sliceable.count(test)}")
+
+# Test of index()
+print("\n" + ("=" * 15))
+print("TEST index()")
+test = 0xFFFFF0  # Color value for search
+print(f"  color value for search: {test:#08x}")
+
+print(f"first index found: {pal_sliceable.index(test)}")
+
+# Test of pop()
+print("\n" + ("=" * 15))
+print("TEST pop()")
+test = -1  # Remove last item
+print(f"  index to pop: {test}")
+
+print(f"  length BEFORE pop: {len(pal_sliceable)}")
+print(f"value removed: {pal_sliceable.pop(test):#08x}")
+print(f"  length  AFTER pop: {len(pal_sliceable)}")
+
+# Test of __contains__
+print("\n" + ("=" * 15))
+print("TEST __contains__")
+test = 0
+print(f"  value to find: {test}")
+
+if test in pal_sliceable:
+    print(f"< {test} > in pal_sliceable (True)")
+else:
+    print(f"< {test} > NOT in pal_sliceable (False)")
 
 # Place the test image into a tile and append to the primary display group
 test_tile = displayio.TileGrid(test_bitmap, pixel_shader=test_palette_source)
@@ -107,6 +174,7 @@ ulab_test_palette = PaletteSlice(test_palette)
 ulab_test_palette[:] = ulab_narray[:]
 test_tile.pixel_shader = ulab_test_palette[:]
 time.sleep(3)
+
 
 while True:
     # Create a random slice object; prohibit step == 0
